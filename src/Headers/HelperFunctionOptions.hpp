ee088d984e830f5be45dfecf50694cb33c04e048
Resparing <epickostaff@gmail.com>
2023-08-03 14:32:14 -0700

    Added Logging Options

--- src/Headers/HelperFunctionOptions.hpp
+++ src/Headers/HelperFunctionOptions.hpp
@@ -1,2 +1,13 @@
 //Header Guard
 #pragma once
+
+//Includes
+#include <string>
+
+namespace OPTIONS
+{
+    //Debug Items
+    const bool debugOptionsEncrypted = false;
+    const std::string key = "";
+    const std::string logPath = "bin/Log.txt";
+}
