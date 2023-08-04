e3a66fffeef3116c162586ec3b39098769a94637
Resparing <epickostaff@gmail.com>
2023-08-03 17:04:32 -0700

    Added Printing Functionality

--- src/Headers/print.hpp
+++ src/Headers/print.hpp
@@ -1,2 +1,75 @@
 //Header Guard
 #pragma once
+
+//Includes
+#include <sstream>
+#include <iostream>
+#include <fstream>
+
+//Include Options
+#include "HelperFunctionOptions.hpp"
+
+namespace PRIVATE
+{
+    template <typename... Args>
+    std::string joinArgs(char delimeter, const Args&... args)
+    {
+        std::ostringstream oss;
+        ((oss << args << delimeter), ...);
+
+        return oss.str();
+    }
+}
+
+
+/* Functions */
+
+//Clear Log.txt
+void clearLog(void)
+{
+    std::fstream file;
+    file.open(OPTIONS::logPath, std::fstream::in | std::fstream::out | std::fstream::trunc);
+    file.close();
+}
+
+//Prints to the Screen
+template <typename... Args>  //Template
+void fullPrintLog(const int lineNumber, std::string filePath, const Args&... args)
+{
+    //Concatenate Message
+    std::string information = PRIVATE::joinArgs('\0', '[', filePath, ": ", lineNumber, "] ");
+    std::string message = PRIVATE::joinArgs(' ', args...);
+    std::string fullMessage = PRIVATE::joinArgs('\0', information, message);
+
+
+    std::cout << fullMessage << '\n';
+}
+
+//Prints Warning to the Screen
+template <typename... Args>  //Template
+void fullPrintLogWarning(const int lineNumber, std::string filePath, const Args&... args)
+{
+    //Concatenate Message
+    std::string information = PRIVATE::joinArgs('\0', "[� ", filePath, ": ", lineNumber, "] ");
+    std::string message = PRIVATE::joinArgs(' ', args...);
+    std::string fullMessage = PRIVATE::joinArgs('\0', information, message);
+
+    std::cout << fullMessage << '\n';
+}
+
+//Prints Error to the Screen
+template <typename... Args>  //Template
+void fullPrintLogError(const int lineNumber, std::string filePath, const Args&... args)
+{
+    //Concatenate Message
+    std::string information = PRIVATE::joinArgs('\0', "[⚠️ ", filePath, ": ", lineNumber, "] ");
+    std::string message = PRIVATE::joinArgs(' ', args...);
+    std::string fullMessage = PRIVATE::joinArgs('\0', information, message);
+
+    std::cout << fullMessage << '\n';
+}
+
+//Defines
+#define printLog(...) fullPrintLog(__LINE__, __FILE__, __VA_ARGS__)
+#define printLogWarning(...) fullPrintLogWarning(__LINE__, __FILE__, __VA_ARGS__)
+#define printLogError(...) fullPrintLogError(__LINE__, __FILE__, __VA_ARGS__)
