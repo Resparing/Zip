b3cb3eced737d8ccee4075597a9fdb9deb67568b
Resparing <epickostaff@gmail.com>
2023-08-23 18:39:14 -0700

    Testing OpenSSL AES

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,15 +1,33 @@
 #include <cstdlib>
-
-#include <zip/typedefs.hpp>
-#include <zip/print.hpp>
-#include <zip/crypt.hpp>
+#include <openssl/aes.h>
+#include <iostream>
+#include <zip/Crypt.hpp>
 
 int main(void)
 {
-    openLog();
 
+    unsigned char cipher[32];
+    const unsigned char userKey[] = "dsadsandjkasfjasdiadskss";
+    const unsigned char input[] = "tLy1IRZUjtLoQxE4tLy1IRZUjtLoQxE4";
 
+    //ENC
+    AES_KEY aesENCKey;
 
-    closeLog();
+    AES_set_encrypt_key(userKey, 128, &aesENCKey);
+    AES_encrypt(input, cipher, &aesENCKey);
+
+    std::cout << cipher << std::endl;
+
+    //DEC
+    AES_KEY aesDEC;
+
+    unsigned char plain[32];
+
+    AES_set_decrypt_key(userKey, 128, &aesDEC);
+    AES_decrypt(cipher, plain, &aesDEC);
+
+    std::cout << plain << std::endl;
+    std::cout << input << std::endl;
+
     return EXIT_SUCCESS;
 }
